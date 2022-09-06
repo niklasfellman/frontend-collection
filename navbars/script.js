e4b498a0d82c07c417963f6af91db5fe8fc3d50c
niklasfellman <purjotti@gmail.com>
2022-09-06 22:01:04 +0300

    update

--- navbars/script.js
+++ navbars/script.js
@@ -1,25 +1,32 @@
-const navBar = document.querySelector(".navigation")
+const navBar = document.querySelectorAll(".navigation")
 const dot = document.querySelector(".dot")
-let active = document.querySelector(".active")
-
-dot.style.transform = `translate(${active.offsetLeft - dot.offsetLeft + active.offsetWidth/2 - dot.offsetWidth/2}px)`	
+let active = document.querySelectorAll(".active")
+console.log(navBar)
+console.log(active)
+dot.style.transform = `translate(${active[0].offsetLeft - dot.offsetLeft + active[0].offsetWidth/2 - dot.offsetWidth/2}px)`	
 dot.style.opacity = `1`	
-
-navBar.addEventListener("mouseover", (x)=>{
-	console.log(x)
+navBar[0].addEventListener("mouseover", (x)=>{
 	if(x.target.localName ==="li"){
 		dot.style.transform = `translate(${x.target.offsetLeft - dot.offsetLeft + x.target.offsetWidth/2 - dot.offsetWidth/2}px)`	
 	}
 })
-
-navBar.addEventListener("mouseout", ()=>{
-	dot.style.transform = `translate(${active.offsetLeft - dot.offsetLeft + active.offsetWidth/2 - dot.offsetWidth/2}px)`	
+navBar[0].addEventListener("mouseout", ()=>{
+	dot.style.transform = `translate(${active[0].offsetLeft - dot.offsetLeft + active[0].offsetWidth/2 - dot.offsetWidth/2}px)`	
+})
+navBar[0].addEventListener("click",(x)=>{
+	if(x.target.localName ==="li"){
+		active[0].classList.remove("active")
+		console.log(x.target)
+		active[0] = x.target
+		active[0].classList.add("active")
+	}
 })
 
-navBar.addEventListener("click",(x)=>{
+navBar[1].addEventListener("click",(x)=>{
 	if(x.target.localName ==="li"){
-		active.classList.remove("active")
-		active = x.target
-		active.classList.add("active")
+			
+		active.item(1).classList.remove("active")
+		//active.item(1) = x.target
+		//active.item(1).classList.add("active")
 	}
 })
